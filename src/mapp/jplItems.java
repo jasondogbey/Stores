a4fffc2f16fea94f013c92887e28e541466c9e2d
emmanuel123456 <emmanuellarry30@yahoo.com>
2018-04-15 15:54:28 +0000

    item update

--- src/mapp/jplItems.java
+++ src/mapp/jplItems.java
@@ -68,9 +68,10 @@ public class jplItems extends JPanel {
         tfStockLevel = new javax.swing.JTextField();
         jScrollPane2 = new javax.swing.JScrollPane();
         taSuppliers = new javax.swing.JTextArea();
-        tfLocation = new javax.swing.JTextField();
         jScrollPane3 = new javax.swing.JScrollPane();
         taNotes = new javax.swing.JTextArea();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        taLocation = new javax.swing.JTextArea();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -143,6 +144,10 @@ public class jplItems extends JPanel {
         taNotes.setRows(5);
         jScrollPane3.setViewportView(taNotes);
 
+        taLocation.setColumns(20);
+        taLocation.setRows(5);
+        jScrollPane4.setViewportView(taLocation);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -181,8 +186,8 @@ public class jplItems extends JPanel {
                                     .addComponent(tfItemName)
                                     .addComponent(tfItemId)
                                     .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE)
-                                    .addComponent(tfLocation)
-                                    .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE))))
+                                    .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE)
+                                    .addComponent(jScrollPane4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE))))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 452, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -208,25 +213,23 @@ public class jplItems extends JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
                     .addComponent(tfStockLevel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(27, 27, 27)
                         .addComponent(jLabel6)
-                        .addGap(31, 31, 31))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addComponent(jLabel7)
+                        .addGap(28, 28, 28))
+                    .addGroup(layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
-                    .addComponent(tfLocation, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(25, 25, 25)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel8)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(jLabel8)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(bnSearch)
                     .addComponent(bnDelete)
@@ -243,7 +246,7 @@ public class jplItems extends JPanel {
     private void bnSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bnSearchActionPerformed
        if (bnSearch.getText().equals("Search")){
             if (tfItemId.getText().isEmpty()){
-                JOptionPane.showMessageDialog(null, "Enter Transaction ID");
+                JOptionPane.showMessageDialog(null, "Enter Item ID");
                 return;  
             }
             String ItemId = tfItemId.getText().trim();
@@ -253,8 +256,10 @@ public class jplItems extends JPanel {
                 ResultSet rs = utility.DBconnection.getStatement().executeQuery(query);
                 while(rs.next()){
                     tfItemId.setText(ItemId);
-                    //cbItem.add(rs.getString("Item_name"));
                     tfItemName.setText(rs.getString("Item_name"));
+                    tfUnitPrice.setText(rs.getString("Unit_price"));
+                    tfStockLevel.setText(rs.getString("Stock_level"));
+                    taSuppliers.setText(rs.getString("Suppliers"));
                 }
             }
         }
@@ -277,12 +282,13 @@ public class jplItems extends JPanel {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTextArea taLocation;
     private javax.swing.JTextArea taNotes;
     private javax.swing.JTextArea taSuppliers;
     private javax.swing.JTextField tfItemId;
     private javax.swing.JTextField tfItemName;
-    private javax.swing.JTextField tfLocation;
     private javax.swing.JTextField tfStockLevel;
     private javax.swing.JTextField tfUnitPrice;
     // End of variables declaration//GEN-END:variables
