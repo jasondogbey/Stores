529ff61c5c58ad6fcda5c21bfdf42a5d72df6741
unknown <unknown@DESKTOP-5VD97JF>
2018-05-13 15:17:01 +0000

    jplchange password completed

--- src/mapp/jplChangePassword.java
+++ src/mapp/jplChangePassword.java
@@ -56,16 +56,16 @@ public class jplChangePassword extends JPanel {
         pfNewPassword = new javax.swing.JPasswordField();
         pfConfirmPassword = new javax.swing.JPasswordField();
 
-        jLabel1.setFont(new java.awt.Font("Calibri", 1, 14)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("BubbleGum", 0, 14)); // NOI18N
         jLabel1.setText("Change User Password");
 
-        jLabel2.setFont(new java.awt.Font("Calibri", 1, 14)); // NOI18N
-        jLabel2.setText("Enter Old Password:");
+        jLabel2.setFont(new java.awt.Font("Californian FB", 1, 14)); // NOI18N
+        jLabel2.setText("Cureent Password:");
 
-        jLabel3.setFont(new java.awt.Font("Calibri", 1, 14)); // NOI18N
-        jLabel3.setText("Enter New Password:");
+        jLabel3.setFont(new java.awt.Font("Californian FB", 1, 14)); // NOI18N
+        jLabel3.setText("New Password:");
 
-        jLabel4.setFont(new java.awt.Font("Calibri", 1, 14)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Californian FB", 1, 14)); // NOI18N
         jLabel4.setText("Confirm Password:");
 
         bnConfirm.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
@@ -84,20 +84,18 @@ public class jplChangePassword extends JPanel {
             }
         });
 
+        pfNewPassword.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pfNewPasswordActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel2)
-                        .addGap(24, 24, 24)
-                        .addComponent(pfNewPassword))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addGap(121, 121, 121)
-                        .addComponent(jLabel1))
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -105,14 +103,23 @@ public class jplChangePassword extends JPanel {
                             .addComponent(jLabel4))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(pfOldPassword, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(pfConfirmPassword)))
+                            .addComponent(pfConfirmPassword)
+                            .addComponent(pfNewPassword)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(54, 54, 54)
                         .addComponent(bnConfirm)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 119, Short.MAX_VALUE)
                         .addComponent(bnCancel)
-                        .addGap(14, 14, 14)))
+                        .addGap(14, 14, 14))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(121, 121, 121)
+                        .addComponent(jLabel1)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel2)
+                        .addGap(18, 18, 18)
+                        .addComponent(pfOldPassword)))
                 .addGap(56, 56, 56))
         );
         layout.setVerticalGroup(
@@ -123,11 +130,11 @@ public class jplChangePassword extends JPanel {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(pfNewPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(pfOldPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(pfOldPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(pfNewPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(30, 30, 30)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(pfConfirmPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -136,7 +143,7 @@ public class jplChangePassword extends JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(bnConfirm)
                     .addComponent(bnCancel))
-                .addContainerGap(22, Short.MAX_VALUE))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -149,17 +156,46 @@ public class jplChangePassword extends JPanel {
             JOptionPane.showMessageDialog(null,"Complete all fieds");
             return;
         }
-        
+        if(!(pfNewPassword.getText().equals(pfConfirmPassword.getText()))){
+           JOptionPane.showMessageDialog(null,"Password fields doesn't match"); 
+           return;
+        }
         
        
-         String s = "update registration set password='"+pfNewPassword.getText()+"' where password='"+pfOldPassword.getText()+"'";
+         String s = "update account set password='"+pfNewPassword.getText()+"' where password='"+pfOldPassword.getText()+"'";
         if(!validation()){
               JOptionPane.showMessageDialog(null,"Complete all fieds");
               initialization();
               return;
          }
+        if(pfNewPassword==pfConfirmPassword){
+           JOptionPane.showMessageDialog(null,"The same"); 
+           return;
+        }
+        
+         try{
+               if (utility.DBconnection.getStatement().executeUpdate(s)>0){
+             
+               JOptionPane.showMessageDialog(null, "Password Change Succesful");
+               pfOldPassword.setText("");
+               pfNewPassword.setText("");
+               pfConfirmPassword.setText("");
+              
+       }else {
+           JOptionPane.showMessageDialog(null, " Password Change Not Successful");
+           initialization();
+       }
+         
+         }catch(Exception e){
+              JOptionPane.showMessageDialog(null,"Error: "+e.getMessage());
+              initialization();
+         }
          
     }//GEN-LAST:event_bnConfirmActionPerformed
+
+    private void pfNewPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pfNewPasswordActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_pfNewPasswordActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
